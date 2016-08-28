d5446952c104683a3101a99d204421e6c4364cd2
Emmanuel <emmanuela.asante@gmail.com>
2016-08-28 12:38:59 +0000

    Implement user signup

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -5,8 +5,6 @@ RSpec.feature "Users signup" do
     visit "/"
 
     click_link "Sign up"
-    fill_in "First name", with: "John"
-    fill_in "Last name", with: "Doe"
     fill_in "Email", with: "john@example.com"
     fill_in "Password",  with: "password"
     fill_in "Password confirmation",  with: "password"
