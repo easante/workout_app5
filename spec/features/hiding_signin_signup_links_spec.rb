8c957b1cbbb01bb0a7d8057c89dd350761d3a098
Emmanuel <emmanuela.asante@gmail.com>
2016-08-28 21:37:40 +0000

    Write hiding signin/signup links feature spec

---
+++ spec/features/hiding_signin_signup_links_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "Hiding signin link" do
+  before do
+    @john = User.create!(email: "john@example.com",
+                         password: "password")
+  end
+
+  scenario "upon successful signin" do
+    visit "/"
+
+    click_link "Sign in"
+    fill_in "Email", with: @john.email
+    fill_in "Password",  with: @john.password
+    click_button "Log in"
+
+    expect(page).to have_link("Sign out")
+    expect(page).not_to have_link("Sign in")
+    expect(page).not_to have_link("Sign up")
+  end
+end
