dabae68eb72275910219f6aab1114c3b1ff9ab1c
Emmanuel <emmanuela.asante@gmail.com>
2016-08-30 17:27:37 +0000

    Create exercise unsuccessfully

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,10 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+  
+  alias_attribute :workout_details, :workout
+  alias_attribute :activity_date, :workout_date
+  
+  validates :duration_in_min, numericality: { greater_than: 0.0 }
+  validates :workout_details, presence: true
+  validates :activity_date, presence: true
 end
--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -4,15 +4,15 @@ RSpec.feature "Creating Exercise" do
   before do
     @john = User.create(email: "john@example.com", password: "password")
     login_as(@john)
-  end
-  
-  scenario "with valid inputs" do
-    visit "/"
     
+    visit "/"
+
     click_link "My Lounge"
     click_link "New Workout"
     expect(page).to have_link("Back")
-    
+  end
+  
+  scenario "with valid inputs" do
     fill_in "Duration", with: 70
     fill_in "Workout details", with: "Weight lifting"
     fill_in "Activity date", with: "2016-07-26"
@@ -26,12 +26,6 @@ RSpec.feature "Creating Exercise" do
   end
   
   scenario "with invalid inputs" do
-    visit "/"
-
-    click_link "My Lounge"
-    click_link "New Workout"
-    expect(page).to have_link("Back")
-
     fill_in "Duration", with: ""
     fill_in "Workout details",  with: ""
     fill_in "Activity date",  with: ""
