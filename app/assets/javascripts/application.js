dcbab9b3a28a13302d1f892b28e6cd2570a4d0ff
Emmanuel <emmanuela.asante@gmail.com>
2016-08-30 21:33:38 +0000

    Add datetime picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui/datepicker
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
+// require turbolinks
 //= require_tree .
---
+++ app/assets/javascripts/exercise.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $('#exercise_workout_date').datepicker({ dateFormat: 'yy-mm-dd' });
+});
