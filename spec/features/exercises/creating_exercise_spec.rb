5423e13ddb8c005def85bc38a4edf3c0a1a3a176
Emmanuel <emmanuela.asante@gmail.com>
2016-08-30 17:11:05 +0000

    Write creating exercise with invalid inputs feature

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -24,5 +24,23 @@ RSpec.feature "Creating Exercise" do
     expect(current_path).to eq(user_exercise_path(@john, exercise))
     expect(exercise.user_id).to eq(@john.id)
   end
+  
+  scenario "with invalid inputs" do
+    visit "/"
+
+    click_link "My Lounge"
+    click_link "New Workout"
+    expect(page).to have_link("Back")
+
+    fill_in "Duration", with: ""
+    fill_in "Workout details",  with: ""
+    fill_in "Activity date",  with: ""
+    click_button "Create Exercise"
+
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Duration in min is not a number")
+    expect(page).to have_content("Workout details can't be blank")
+    expect(page).to have_content("Activity date can't be blank")
+  end
 
 end
