c49908feff9a0fd0839765721b6dde40e9bac219
Emmanuel <emmanuela.asante@gmail.com>
2016-08-31 06:51:26 +0000

    Fix hard-coded date in creating exercise spec

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -15,7 +15,7 @@ RSpec.feature "Creating Exercise" do
   scenario "with valid inputs" do
     fill_in "Duration", with: 70
     fill_in "Workout details", with: "Weight lifting"
-    fill_in "Activity date", with: "2016-07-26"
+    fill_in "Activity date", with: 3.days.ago
     click_button "Create Exercise"
     
     expect(page).to have_content("Exercise has been created")
