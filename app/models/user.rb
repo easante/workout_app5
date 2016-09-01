ec1a38413363ba9387da05d49322aad40fa0ecd5
Emmanuel <emmanuela.asante@gmail.com>
2016-09-01 03:59:45 +0000

    Add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+         
   has_many :exercises
   
   def full_name
