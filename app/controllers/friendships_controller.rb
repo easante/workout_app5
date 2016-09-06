1f6161e137a4cbc58a5a6ea3c09d3cb24d91033a
Emmanuel <emmanuela.asante@gmail.com>
2016-09-06 22:03:16 +0000

    Implement showing friend workouts

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,11 @@
 class FriendshipsController < ApplicationController
   before_action :authenticate_user!
   
+  def show
+    @friend = Friendship.find(params[:id]).friend
+    @exercises = @friend.exercises
+  end
+  
   def create
     friend = User.find(params[:friend_id])
     params[:user_id] = current_user.id
