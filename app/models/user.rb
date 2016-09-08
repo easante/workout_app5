6f1ef589e5ba8cb95368a57437c6297b8dc891ce
Emmanuel <emmanuela.asante@gmail.com>
2016-09-08 18:50:11 +0000

    Create chatroom when user signs up

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
   has_many :exercises
   has_many :friendships
   has_many :friends, through: :friendships, class_name: "User"
+  has_one :room
+  
+  after_create :create_chatroom
   
   self.per_page = 10
   
@@ -37,5 +40,12 @@ class User < ApplicationRecord
   
   def current_friendship(friend)
     friendships.where(friend: friend).first
+  end
+  
+  private
+  
+  def create_chatroom
+    hyphenated_username = self.full_name.split.join('-')
+    Room.create(name: hyphenated_username, user_id: self.id)
   end
 end
--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -14,8 +14,15 @@ RSpec.feature "Users signup" do
 
     expect(page).to have_content("You have signed up successfully.")
     
+    user = User.last
+    room = user.room
+    room_name = user.full_name.split.join('-')
+    expect(room.name).to eq(room_name)
+    
+    
     visit "/"
     expect(page).to have_content("John Doe")
+  
   end
   
   scenario "with invalid credentials" do
