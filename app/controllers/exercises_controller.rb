a155665e91e325398881ebc419775dfcdcbdaed8
Emmanuel <emmanuela.asante@gmail.com>
2016-09-09 19:41:55 +0000

    Wire-up frontend to backend

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -7,7 +7,7 @@ class ExercisesController < ApplicationController
     @friends = current_user.friends
     set_current_room
     @message = Message.new
-    @messages = current_room.messages if current_room
+    @messages = current_room.messages.reverse if current_room
     @followers = Friendship.where(friend_id: current_user.id)
   end
   
