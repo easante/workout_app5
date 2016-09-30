04d1881f99997286a1640b0e8f07f658db4a0205
Emmanuel <emmanuela.asante@gmail.com>
2016-09-30 12:30:02 +0000

    Add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
          
   has_many :exercises
   
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
   def full_name
     [first_name, last_name].join(" ")
   end
