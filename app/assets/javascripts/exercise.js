c0446ac74d314afe68d5e43e4b9d61982eb48131
Emmanuel <emmanuela.asante@gmail.com>
2016-10-03 09:54:49 +0000

    Implement showing friend workouts

--- app/assets/javascripts/exercise.js
+++ app/assets/javascripts/exercise.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#exercise_workout_date").datepicker({ dateFormat: 'yy-mm-dd' });
   
-  var regex = /\/users\/\d+\/exercises$/i;
+  var regex = /\/users\/\d+\/exercises$|\/friendships\/d+$/i;
 
   if($(location).attr('pathname').match(regex)) {
     drawChart();
