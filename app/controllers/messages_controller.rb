7a6e36d41f8856755faafb576a08867e94bd72a2
Emmanuel <emmanuela.asante@gmail.com>
2016-10-12 12:29:17 +0000

    Fix issue with rails port and user url

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,8 +8,7 @@ class MessagesController < ApplicationController
     if @message.save
       respond_to do |format|
         format.html { redirect_to user_exercises_path(current_user) }
-        format.js { ActionCable.server.broadcast "messages_room_#{current_room.id}",
-        render(partial: 'shared/message', object: @message) }
+        format.js { ActionCable.server.broadcast "messages_room_#{current_room.id}", render(partial: 'shared/message', object: @message) }
       end
     end
   end
